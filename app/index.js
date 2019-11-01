9e2cfdf04d36facccf22764fd27d75137117fc57
adambirse <adambirse@gmail.com>
2019-11-01 14:24:07 +0000

    just for Ian

--- app/index.js
+++ app/index.js
@@ -4,6 +4,6 @@
  * @param {Object} req Cloud Function request context.
  * @param {Object} res Cloud Function response context.
  */
-exports.helloGET = function helloGET (req, res) {
+exports.helloGET = (req, res) => {
     res.send(`Hello ${req.body.name || 'World'}!`);
 };
